3d827c836a68d6c3dd7e1be87dea00d6997178ae
Juturu-Pradeep-Kumar-Reddy <Juturu.Pradeep.Kumar.Reddy@ibm.com>
2026-09-19 16:48:58 +0530

    feat: Added Pradeep portfolio

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/PradeepPortfolio/",
     plugins: [react()],
 });
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/PradeepPortfolio/",
   plugins: [react()],
 })
